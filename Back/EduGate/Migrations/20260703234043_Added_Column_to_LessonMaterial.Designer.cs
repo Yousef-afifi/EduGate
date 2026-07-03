800f393548ed2ddd2d62d168715d29edf82b4b80
AboElHareth <m.aboelhareth.ali@gmail.com>
2026-07-04 02:58:41 +0300

    Edited Database

--- Back/EduGate/Migrations/20260703234043_Added_Column_to_LessonMaterial.Designer.cs
+++ Back/EduGate/Migrations/20260703234043_Added_Column_to_LessonMaterial.Designer.cs
@@ -4,6 +4,7 @@ using EduGate.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EduGate.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260703234043_Added_Column_to_LessonMaterial")]
+    partial class Added_Column_to_LessonMaterial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Back/EduGate/Models/LessonMaterial.cs
+++ Back/EduGate/Models/LessonMaterial.cs
@@ -5,8 +5,10 @@ namespace EduGate.Models
     public class LessonMaterial
     {
         public int Id { get; set; }
+        public string Title { get; set; }
         public string File_Path { get; set; }
         public string File_Type { get; set; }
+        public double File_Size { get; set; }
         [ForeignKey("lesson")]
         public int Lesson_Id { get; set; }
         public Lesson lesson { get; set; }
